a8255b223faa79808d575b2721d87edfeb85cab7
MarcelCG <marcel.castro@ucr.ac.cr>
2023-09-07 19:21:21 -0600

    Finalización del laboratorio 3. Había un pequeño problema de compilación pero ya en teoría todo anda bien.

---
+++ Laboratorio3/Controllers/CancionController.cs
@@ -0,0 +1,28 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio3.Controllers
+{
+    public class CancionController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cancion = GetCancion();
+            ViewBag.MainTitle = "Mi canción favorita";
+            return View(cancion);
+        }
+      private CancionModel GetCancion()
+        {
+            var cancion = new CancionModel();
+            cancion.Nombre = "Speedometer";
+            cancion.Autor = "Post Malone";
+            cancion.Album = "Austin";
+            cancion.Fecha = new DateTime(2023, 07, 28);
+            cancion.Genero = "Synth-pop";
+
+            return cancion;
+        }
+    }
+}
---
+++ Laboratorio3/Models/CancionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio3.Models
+{
+    public class CancionModel
+    {
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string Album { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Genero { get; set; }
+    }
+}
